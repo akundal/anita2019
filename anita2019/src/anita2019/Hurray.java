ae61541b098cc7768f167dd294ec4d84ed122407
akundal <akundal@CSCUSAAH774255>
2019-12-12 17:48:10 -0500

    my fifth commit from anita2019 project

---
+++ anita2019/src/anita2019/Hurray.java
@@ -0,0 +1,10 @@
+package anita2019;
+
+public class Hurray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World this is my fifth project");
+	}
+
+}
